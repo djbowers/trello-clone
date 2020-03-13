2ea66eab63f8f71a6a8b4d701d85e75dc2f8307b
DJ Bowers <dj@dj-bowers.com>
2020-03-13 16:43:54 -0700

    install redux logger
    dependencies:
    * redux-logger

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,15 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+
 import rootReducer from '../reducers'
 
-const store = createStore(rootReducer)
+const middlewares = []
+
+if (process.env.NODE_ENV === `development`) {
+  const { logger } = require(`redux-logger`)
+
+  middlewares.push(logger)
+}
+
+const store = compose(applyMiddleware(...middlewares))(createStore)(rootReducer)
 
 export default store
