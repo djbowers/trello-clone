bb09b170725258784d535f3230d12af105bc1de8
DJ Bowers <dj@dj-bowers.com>
2020-03-16 09:03:08 -0700

    Updates

---
+++ precache-manifest.04a9e981b8a79126ac915f12ff9c84bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a970fd299d034943df3577a9704cce4e",
+    "url": "/trello-clone/index.html"
+  },
+  {
+    "revision": "1b13cbae43aa18f19b28",
+    "url": "/trello-clone/static/css/main.d9fc5c6d.chunk.css"
+  },
+  {
+    "revision": "6c381b0113e4a63745e5",
+    "url": "/trello-clone/static/js/2.0613c769.chunk.js"
+  },
+  {
+    "revision": "c099810c3c102db7a44755001529c804",
+    "url": "/trello-clone/static/js/2.0613c769.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b13cbae43aa18f19b28",
+    "url": "/trello-clone/static/js/main.332110f1.chunk.js"
+  },
+  {
+    "revision": "01d9cced60e505c9249c",
+    "url": "/trello-clone/static/js/runtime-main.17cd50a1.js"
+  }
+]);
